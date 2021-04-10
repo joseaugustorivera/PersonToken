6021311d4a65873de1414bc11b6eda35f3bf4eeb
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-04-10 00:41:00 -0400

    Se adiciono el controlador People con token.

---
+++ PersonToken/PersonToken/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonToken.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
